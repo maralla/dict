16aeb7546b662326c0c9c34346cbc9ddcb4330fb
Maralla <wei.zhang@ele.me>
2013-07-25 14:08:53 +0800

    changed setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name='Dictionary',
       version='1.0',
-      description='OpenShift App',
+      description='Oxford dictionary clone',
       author='Maralla',
       author_email='maralla.ai@gmail.com',
-      url='http://www.python.org/sigs/distutils-sig/',
+      url='http://t-maralla.rhcloud.com/',
       install_requires=['Flask>=0.7.2', 'mongokit', 'lxml'],
      )
